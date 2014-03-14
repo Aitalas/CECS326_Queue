2ab5b4bde6e8d40ca1a5a428d3b194566a77df07
Aitalas <aitalas@ubuntu.(none)>
2014-03-13 19:16:38 -0700

    first commit

---
+++ banner.cpp
@@ -0,0 +1,64 @@
+/* 
+Commands to run in terminal:
+g++ banner.cpp -o b.out
+*/
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <cstring>
+#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+
+	//init queue
+	int qid = msgget(ftok(".",'u'), IPC_EXCL|IPC_CREAT|0600);
+
+	//init message struct
+	struct buf {
+		long mtype; // required
+		char greeting[50]; // mesg content
+	};
+
+	buf msg;
+	int size = sizeof(msg)-sizeof(long);
+
+	//init booleans
+	bool c1 = true;
+	bool c2 = true;
+	bool c3 = true;
+
+	cout << getpid() << ": Waiting for divisible numbers..." << endl;
+
+	//check top of queue
+	while (c1) {
+		//get top of queue
+		msgrcv(qid, (struct msgbuf *)&msg, size, 0, 0); 
+
+		//check if fake
+		if (msg.mtype != 101 || msg.mtype != 102) {
+			continue;
+		} 
+		//check if real
+		else if (msg.mtype == 101) {
+			//print message
+			cout << getpid() << ": " << msg.greeting << endl;
+		}
+
+		//check if catcher end
+		else if (msg.mtype == 102) {
+			c1 = false;
+		} 
+	}
+
+	// now safe to delete message queue
+	msgctl (qid, IPC_RMID, NULL);
+
+	exit(0);
+}
+
+
---
+++ catcher.cpp
@@ -0,0 +1,71 @@
+/* 
+Commands to run in terminal:
+g++ randgen.o catcher.cpp -o c1.out
+*/
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <cstring>
+#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <cstdlib>
+#include "randgen.h"
+
+using namespace std;
+
+int main() {
+
+	//init queue
+	int qid = msgget(ftok(".",'u'), 0);
+
+	// declare my message buffer
+	struct buf {
+		long mtype; // required
+		char greeting[50]; // mesg content
+	};
+	buf msg;
+	int size = sizeof(msg)-sizeof(long);
+
+	//init prime number
+	int PRIME_NUM = 3;
+
+	//init random number
+	init();
+	int randNum = randgen(qid);
+
+	cout << getpid() << ": Generating divisible numbers..." << endl;
+
+	//check if divisible
+	//while random number is greater than 100
+	while (randNum > 100) {
+		//if randNum is divisible by the prime number
+		if (randNum % PRIME_NUM == 0) {
+			cout << getpid() << ": Got a number!" << endl;
+
+			//set mtype
+			msg.mtype = 101;
+			
+			//set greeting
+			strcpy(msg.greeting, "Number is divisible!");
+
+			//send message
+			msgsnd(qid, (struct msgbuf *)&msg, size, 0); 
+
+			//notify user
+			cout << getpid() << ": Notification sent." << endl;
+		}
+		cout << getpid() << ": No good number..." << endl;
+	}
+
+	//send terminating message
+	msg.mtype = 102;
+	strcpy(msg.greeting, "Catcher 1 ending.");
+	msgsnd(qid, (struct msgbuf *)&msg, size, 0); 
+	
+	cout << getpid() << ": Catcher 1 ending." << endl;
+	exit(0);
+}
+
+
