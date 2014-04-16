878d23aa1d47476f944492abd065fa9f7b9c9bc8
Aitalas <radicalcyn@yahoo.com>
2014-04-16 10:37:19 -0700

    Update ctest2.cpp

--- ctest2.cpp
+++ ctest2.cpp
@@ -1,7 +1,7 @@
 //Cynthia Nguyen
 //CECS326
 //Programming: Message Queue
-//ctest2.cpp
+//ctest.cpp
 //Description: This is an event catcher program that sends a message to the 
 //	banner's queue if certain conditions are met. Once the conditions are
 //	no longer true, the event catcher stops by sending a terminating 
@@ -35,7 +35,7 @@ int main() {
 	int size = sizeof(msg)-sizeof(long);
 
 	//initialize prime number
-	int primeNum = 5;
+	int primeNum = 3;
 
 	//initialize random number
 	int randomNum = randgen(qid);
@@ -44,7 +44,7 @@ int main() {
 	string temp_str = strs.str();
 	const char* numChar = temp_str.c_str();
 
-	cout << getpid() << ": Beginning Catcher 2..." << endl;
+	cout << getpid() << ": Beginning Catcher 1..." << endl;
 
 	while (randomNum > 100) {
 		if (randomNum % primeNum == 0) {
@@ -54,13 +54,13 @@ int main() {
 			//convert randomNum to char
 			strs.str("");//reset stringstream
 			strs.clear();
-			strs << randomNum;//convert number
+			strs << randomNum;//number to convert
 			temp_str = strs.str();//convert to string
 			numChar = temp_str.c_str();//convert to char
 			strcpy(msg.greeting, numChar);//copy to greeting
 
 			//concatenate rest of the message
-			strcat(msg.greeting, " is divisible by 5.");
+			strcat(msg.greeting, " is divisible by 3.");
 
 			//send message
 			msg.mtype = 2;
@@ -74,7 +74,7 @@ int main() {
 
 
 	//send terminating message
-	msg.mtype = 92;
+	msg.mtype = 91;
 	strcpy(msg.greeting, "Catcher 2 has ended.");
 	msgsnd(qid, (struct msgbuf *)&msg, size, 0); 
 	cout << getpid() << ": Sent final message." << endl;
